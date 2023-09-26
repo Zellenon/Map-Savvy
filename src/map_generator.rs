8719b3a71e61524749a51c534dbe133bf2fd7c51
Zellenon <zellenon@gmail.com>
2023-09-25 20:28:34 -0500

    final implementation

--- src/map_generator.rs
+++ src/map_generator.rs
@@ -19,13 +19,15 @@ const BLUE: [u8; 49] = [
     200, 195, 190, 185, 180, 175,
 ];
 
+pub type WorldMap = Vec<Vec<usize>>;
+
 #[derive(Clone)]
 pub struct MapData {
     pub faults: Vec<Fault>,
     pub seed: u32,
     pub seed_name: String,
     pub percent_water: f64,
-    pub size: (u32, u32),
+    pub size: (usize, usize),
 }
 
 impl Default for MapData {
@@ -35,7 +37,7 @@ impl Default for MapData {
             seed: 0,
             seed_name: String::new(),
             percent_water: 0.6,
-            size: (3000, 2000),
+            size: (1800, 1200),
         }
     }
 }
@@ -71,69 +73,13 @@ fn map_color(i: usize) -> image::Rgb<u8> {
 }
 
 pub fn map_image(data: &MapData) -> Result<egui::ColorImage, ImageError> {
-    let image_width = data.size.0 as f64;
-    let image_height = data.size.1 as f64;
+    let world_heights = map_from_faults(&data.size, data.faults.as_slice());
 
-    let y_range_div_2 = (image_height as f64) / 2.;
-    let y_range_div_pi = (image_height as f64) / PI;
-    let sin_iter_phi = |x: f64| (x * 2. * PI / image_width).sin();
+    let color_map = colormap_from_worldmap(world_heights, data.percent_water);
 
-    let world_shape: Vec<Vec<(u32, u32)>> = (0..data.size.0)
-        .map(|w| (0..data.size.1).map(|w2| (w, w2)).collect())
-        .collect();
-
-    let world_heights: Vec<Vec<f64>> = world_shape
-        .par_iter()
-        .enumerate()
-        .map(|(x, column)| {
-            let flag_theta: Vec<(bool, f64)> = data
-                .faults
-                .par_iter()
-                .map(|fault_i| {
-                    let sin_iter_index = image_width * (fault_i.xsi + fault_i.shift) - x as f64;
-                    let atan_args = sin_iter_phi(sin_iter_index) * fault_i.tan_b;
-                    (
-                        fault_i.flag,
-                        y_range_div_pi * atan_args.atan() + y_range_div_2, // Theta
-                    )
-                })
-                .collect();
-            column
-                .par_iter()
-                .enumerate()
-                .map(|(y, _)| {
-                    flag_theta
-                        .par_iter()
-                        .map(|(a, theta)| {
-                            let b = y as f64 <= *theta;
-                            // This should be XOR, but this is the same and faster
-                            if *a == b {
-                                1.
-                            } else {
-                                -1.
-                            }
-                        })
-                        .sum()
-                })
-                .collect()
-        })
-        .collect();
-    println!("Finished calculating world heights");
-
-    let world_max = world_heights
-        .iter()
-        .map(|w| w.iter().cloned().fold(f64::NEG_INFINITY, f64::max))
-        .fold(f64::NEG_INFINITY, f64::max);
-    let world_min = world_heights
-        .iter()
-        .map(|w| w.iter().cloned().fold(f64::INFINITY, f64::min))
-        .fold(f64::INFINITY, f64::min);
-    let world_range = world_max - world_min;
-
-    let mut imgbuf = image::ImageBuffer::new(data.size.0, data.size.1);
+    let mut imgbuf = image::ImageBuffer::new(data.size.0 as u32, data.size.1 as u32);
     imgbuf.enumerate_pixels_mut().for_each(|(x, y, pixel)| {
-        let i = (world_heights[x as usize][y as usize] as f64 - world_min) / world_range * 30.;
-        *pixel = map_color(i as usize);
+        *pixel = map_color(color_map[x as usize][y as usize]);
     });
     println!("Colors set");
 
@@ -141,7 +87,7 @@ pub fn map_image(data: &MapData) -> Result<egui::ColorImage, ImageError> {
     // let size = [imgbuf.width() as _, imgbuf.height() as _];
     // let pixels = imgbuf.as_flat_samples();
     // ColorImage::from_rgba_unmultiplied(size, pixels.as_slice())
-    let image = image::io::Reader::open("fractal.png")?.decode()?;
+    let image = image::io::Reader::open("map.png")?.decode()?;
     let size = [image.width() as _, image.height() as _];
     let image_buffer = image.to_rgba8();
     let pixels = image_buffer.as_flat_samples();
@@ -149,4 +95,105 @@ pub fn map_image(data: &MapData) -> Result<egui::ColorImage, ImageError> {
         size,
         pixels.as_slice(),
     ))
+}
+
+pub fn make_faults(count: usize) -> Vec<Fault> {
+    (0..count).into_iter().map(|_| Fault::new()).collect()
+}
+
+pub fn map_from_faults(size: &(usize, usize), faults: &[Fault]) -> WorldMap {
+    let image_width = size.0 as f64;
+    let image_height = size.1 as f64;
+
+    let y_range_div_2 = (image_height) / 2.;
+    let y_range_div_pi = (image_height) / PI;
+    let sin_iter_phi = |x: f64| (x * 2. * PI / image_width).sin();
+
+    let world_shape: Vec<Vec<(usize, usize)>> = (0..size.0)
+        .map(|w| (0..size.1).map(|w2| (w, w2)).collect())
+        .collect();
+
+    world_shape
+        .par_iter()
+        .enumerate()
+        .map(|(x, column)| {
+            let flag_theta: Vec<(bool, usize)> = faults
+                .par_iter()
+                .map(|fault_i| {
+                    let sin_iter_index = image_width * (fault_i.xsi + fault_i.shift) - x as f64;
+                    let atan_args = sin_iter_phi(sin_iter_index) * fault_i.tan_b;
+                    (
+                        fault_i.flag,
+                        (y_range_div_pi * atan_args.atan() + y_range_div_2) as usize, // Theta
+                    )
+                })
+                .collect();
+            let temp = column
+                .par_iter()
+                .enumerate()
+                .map(|(y, _)| {
+                    flag_theta
+                        .par_iter()
+                        .map(|(a, theta)| {
+                            // This should be XOR, but this is the same and faster
+                            if *a == (y <= *theta) {
+                                1
+                            } else {
+                                0
+                            }
+                        })
+                        .sum()
+                })
+                .collect();
+            temp
+        })
+        .collect()
+}
+
+pub fn colormap_from_worldmap(map: WorldMap, percent_water: f64) -> WorldMap {
+    let world_min = map
+        .iter()
+        .map(|w| w.iter().cloned().fold(usize::MAX, usize::min))
+        .fold(usize::MAX, usize::min);
+
+    let world_heights: WorldMap = map
+        .par_iter()
+        .map(|cols| cols.par_iter().map(|cell| cell - world_min).collect())
+        .collect();
+
+    let world_max = world_heights
+        .iter()
+        .map(|w| w.iter().cloned().fold(usize::MIN, usize::max))
+        .fold(usize::MIN, usize::max);
+
+    let threshhold = {
+        let mut threshhold: usize = 0;
+        let total: usize =
+            ((percent_water * map.iter().count() as f64) * map[0].iter().count() as f64) as usize;
+        while world_heights
+            .par_iter()
+            .map(|cols| cols.par_iter().filter(|cell| **cell < threshhold).count())
+            .sum::<usize>()
+            < total
+        {
+            threshhold = threshhold + 1;
+        }
+        threshhold
+    };
+
+    let over_threshhold = world_max - threshhold;
+    world_heights
+        .par_iter()
+        .map(|cols| {
+            cols.par_iter()
+                .map(|cell| {
+                    if *cell < threshhold {
+                        (cell * 15) / threshhold
+                    } else {
+                        (cell - threshhold) * 15 / (over_threshhold) + 16
+                    }
+                })
+                .collect()
+        })
+        .collect()
 }
