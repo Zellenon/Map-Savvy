32cf862aa1c5c8090cfbd33ccb02526ad2bf679f
Zellenon <zellenon@gmail.com>
2025-05-02 16:23:04 -0500

    moved out old code

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use app::MyApp;
-use eframe::egui;
+use eframe::NativeOptions;
 
 mod app;
 mod map_generator;
 
 fn main() -> Result<(), eframe::Error> {
-    let options = eframe::NativeOptions {
-        ..Default::default()
-    };
     eframe::run_native(
-        "Custom window frame", // unused title
-        options,
+        "Map Savvy",
+        NativeOptions::default(),
         Box::new(|_cc| Ok(Box::new(MyApp::default()))),
     )
 }
